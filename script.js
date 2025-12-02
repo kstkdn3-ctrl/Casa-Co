9c6d85bf83fa449c1e7068f6a935bb94c3504f2b
kstkdn3-ctrl <ks.tk.dn3@gmail.com>
2025-12-02 20:59:40 +0200

    add table

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ document.querySelectorAll(".card").forEach(card => {
         window.location.href = card.dataset.link;
     });
 });
+const buttons = document.querySelectorAll('.text-btn');
+
+  buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      buttons.forEach(b => b.classList.remove('active'));
+      btn.classList.add('active');
+    });
+  });
