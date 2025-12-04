8049e4366ef584d46ecfe7010dc800c9b07f8f29
kstkdn3-ctrl <ks.tk.dn3@gmail.com>
2025-12-04 21:12:40 +0200

    add reviews

--- script.js
+++ script.js
@@ -11,3 +11,34 @@ const buttons = document.querySelectorAll('.text-btn');
       btn.classList.add('active');
     });
   });
+document.getElementById("char").addEventListener("click", function () {
+  document.getElementById("char1").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+document.getElementById("delivery").addEventListener("click", function () {
+  document.getElementById("delivery1").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+document.getElementById("reviews").addEventListener("click", function () {
+  document.getElementById("reviews1").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+let images = [];
+let currentIndex = 0;
+
+const productImages = document.querySelectorAll(".image-box img");
+
+productImages.forEach((img, i) => {
+    images.push(img.src);     
+    img.dataset.index = i;    
+});
+
+productImages.forEach(img => {
+    img.addEventListener("click", () => {
+        currentIndex = parseInt(img.dataset.index);
+        openModal();
+    });
+});
